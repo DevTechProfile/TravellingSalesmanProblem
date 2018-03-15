9fc730dbe51970f0610246c3070ae55252c51dbd
Marc Fangmeyer <marc.fangmeyer@gmx.de>
2018-03-15 18:27:30 +0100

    Preparing PathGenertorFactory including 3D pathes. Want to try 3D TSP.

---
+++ PathHelper/EPathType.cs
@@ -0,0 +1,30 @@
+﻿namespace PathHelper
+{
+    public enum TspPathType
+    {
+        /// <summary>
+        /// Uniform distributed 2D point cloud
+        /// </summary>
+        Uniform2DRandom,
+
+        /// <summary>
+        /// Normal distributed 2D point cloud
+        /// </summary>
+        Normal2DRandom,
+
+        /// <summary>
+        /// Uniform distributed 3D point cloud
+        /// </summary>
+        Uniform3DRandom,
+
+        /// <summary>
+        /// Normal distributed 3D point cloud
+        /// </summary>
+        Normal3DRandom,
+
+        /// <summary>
+        /// Circle path
+        /// </summary>
+        Ciclre
+    }
+}
